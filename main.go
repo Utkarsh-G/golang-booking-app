391ff11340db52fb9d74e035b240f9f08497547d
Utkarsh Gaur <charmedtobe@gmail.com>
2022-12-16 15:26:07 -0500

    Hellow World

---
+++ main.go
@@ -0,0 +1,15 @@
+// Initialize project in terminal with `go mod init golang-booking-app`
+
+package main
+
+import (
+	"fmt"
+)
+
+// declare an entrypoint, ie main. 1 main per application
+func main() {
+	fmt.Print("Hello World")
+	fmt.Println("hello again. whole line to myself")
+}
+
+// run it with `go run main.go`
